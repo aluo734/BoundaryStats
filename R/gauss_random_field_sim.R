e58217fea6b7a2bd21af4d48ea033eca63750a95
aluo734 <amy.luo.15@gmail.com>
2024-10-19 21:55:17 -0400

    added LISA clusters function within package

--- R/gauss_random_field_sim.R
+++ R/gauss_random_field_sim.R
@@ -4,6 +4,7 @@
 #' input raster, using the same spatial autocorrelation range as the input
 #'
 #' @param x A SpatRaster object.
+#' @param corr_range The range of spatial autocorrelation to simulate. Can be estimated using the lisa_clusters function.
 #' @return A SpatRaster object with boundary elements.
 #'
 #' @examples \donttest{
@@ -11,7 +12,8 @@
 #' grassland <- terra::rast(grassland_matrix, crs = grassland_crs)
 #' terra::ext(grassland) <- grassland_ext
 #'
-#' simulation <- gauss_random_field_sim(grassland)
+#' corr <- lisa_clusters(grassland)
+#' simulation <- gauss_random_field_sim(grassland, corr)
 #' terra::plot(simulation)
 #' }
 #'
@@ -19,43 +21,7 @@
 #' @references
 #' James, P. M. A., Fleming, R.A., & Fortin, M.-J. (2010) Identifying significant scale-specific spatial boundaries using wavelets and null models: Spruce budworm defoliation in Ontario, Canada as a case study. Landscape Ecology, 6, 873-887.
 #' @export
-gauss_random_field_sim <- function (x) {
-  # estimate autocorrelation range using local Moran's I + LISA clustering
-  polys <- terra::as.polygons(x, dissolve = F) %>%
-    sf::st_as_sf() %>%
-    sf::st_buffer(0.01)
-  names(polys)[1] <- 'values'
-  
-  x_cluster <- polys %>%
-    dplyr::mutate(nb = sfdep::st_contiguity(geometry),
-                  wt = sfdep::st_weights(nb, allow_zero = T),
-                  local_moran = sfdep::local_moran(values, nb, wt)) %>%
-    tidyr::unnest(local_moran) %>%
-    dplyr::mutate(cluster = ifelse(p_ii <= 0.05, 1, NA)) %>%
-    dplyr::select(cluster, geometry) %>%
-    terra::rasterize(x, field = 'cluster') %>%
-    terra::as.polygons(na.rm = T) %>%
-    terra::buffer(., 0.01) %>%
-    terra::disagg() %>%
-    sf::st_as_sf()
-
-  lengths <- c()
-  for (i in 1:nrow(x_cluster)) {
-    lengths <- sf::st_geometry(x_cluster[i,]) %>%
-      sf::st_cast(., "POINT") %>%
-      sf::st_distance() %>%
-      max() %>%
-      append(lengths, .) %>%
-      as.numeric()
-  }
-  median_length <- median(lengths)
-
-  cell_size <- terra::cellSize(x, transform = T) %>%
-    terra::values() %>%
-    mean
-  corr_range <- median_length/sqrt(cell_size)
-
-  # simulate raster
+gauss_random_field_sim <- function (x, corr_range) {
   repeat {
     invisible(capture.output(
       x_sim <- try(list(1:terra::nrow(x), 1:terra::ncol(x)) %>%
